f9dcda77e4fae7487cbe801eef5f082c007eb6ec
mamhesam <mamhesam1@gmail.com>
2023-01-12 17:15:55 +0330

    add all dependecy

--- app/src/main/java/ir/mamhesam/cardgamejetpack/MainActivity.kt
+++ app/src/main/java/ir/mamhesam/cardgamejetpack/MainActivity.kt
@@ -3,13 +3,6 @@ package ir.mamhesam.cardgamejetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ir.mamhesam.cardgamejetpack.ui.theme.CardGameJetpackTheme
 
 class MainActivity : ComponentActivity()
@@ -19,29 +12,9 @@ class MainActivity : ComponentActivity()
         super.onCreate(savedInstanceState)
         setContent {
             CardGameJetpackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize() ,
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name : String)
-{
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview()
-{
-    CardGameJetpackTheme {
-        Greeting("Android")
-    }
-}
